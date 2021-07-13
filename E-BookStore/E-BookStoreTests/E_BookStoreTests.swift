e5ff8469f1777272aaf55abdd2332d21ca4bce0d
Naveed <naveed.technerds@gmail.com>
2021-07-13 13:03:31 +0500

    pod installation

---
+++ E-BookStore/E-BookStoreTests/E_BookStoreTests.swift
@@ -0,0 +1,33 @@
+//
+//  E_BookStoreTests.swift
+//  E-BookStoreTests
+//
+//  Created by Muhammad  Naveed on 09/07/2021.
+//
+
+import XCTest
+@testable import E_BookStore
+
+class E_BookStoreTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ E-BookStore/Pods/Target Support Files/Pods-E-BookStore-E-BookStoreUITests/Pods-E-BookStore-E-BookStoreUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_E_BookStore_E_BookStoreUITests : NSObject
+@end
+@implementation PodsDummy_Pods_E_BookStore_E_BookStoreUITests
+@end
